7f8c6134d9152d8203e469a033b78b6ce9768859
Mateo <mtaycruz@gmail.com>
2021-05-05 12:40:30 -0400

    module 1 completed

---
+++ client/build/precache-manifest.885628fbc10c841ea6a67d0aece18927.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0effa3bf47f0b9351854836d82f0db01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c21b301eaa1c31ead36b",
+    "url": "/static/css/main.e7186c00.chunk.css"
+  },
+  {
+    "revision": "68506020324682700d89",
+    "url": "/static/js/2.7753db30.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.7753db30.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c21b301eaa1c31ead36b",
+    "url": "/static/js/main.15bc5366.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
